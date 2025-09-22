96f1cf02c1183b8514cfdcd9cb6b5caae24dae19
Star <101126792+Starx707@users.noreply.github.com>
2025-09-22 13:24:19 +0200

    Added some psuedocode in index

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header> <!--remove later on to put into a seperate file-->
+
+    </header>
+    <main>
+        <section id="RecentBagesSec">
+<!--Span or so for the 'title'-->
+<!--div (for every badge)-->
+    <!--link to the badge page and "opening" the right badge-->
+        <!--badge image-->
+        </section>
+        <section id="FavoritePlantsSec">
+<!--div-->
+   
+        </section>
+        <section id="PlantStatusSec">
+<!--div-->
+    <!---->
+
+
+        </section>
+    </main>
+</body>
+</html>
