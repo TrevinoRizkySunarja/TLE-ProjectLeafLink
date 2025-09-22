9b87d450b21ed9a47b987a4eb0138f46cfee052d
chrisieee <christa_pol1@hotmail.com>
2025-09-22 13:23:33 +0200

    Home update

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
 <?php
+session_start();
+
+if (isset($_SESSION["Login"])) {
+    $first_name = $_SESSION['first_name'];
+} else {
+    $first_name = "undefined";
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Includes/CSS/style.css">
+    <link rel="stylesheet" href="Includes/CSS/home.css">
+    <title>Home</title>
+</head>
+<body>
+<header>
+    <H1>Home</H1>
+</header>
+<main>
+    <section class="collection">
+        <div id="shelfs">
+            <img src="Includes/images/Shelf.png" alt="shelf">
+            <img src="Includes/images/Shelf.png" alt="shelf">
+        </div>
+    </section>
+</main>
+</body>
+</html>
