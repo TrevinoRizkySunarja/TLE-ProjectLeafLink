747ece502b094b9e2586e8f140a05538f4cc4850
anemoia <killboy1100@gmail.com>
2025-09-22 13:24:55 +0200

    added some stuff to the Archief
    Took 10 minutes

--- archief.php
+++ archief.php
@@ -11,5 +11,15 @@
     <script src="Includes/JS/archief.js" defer></script>
 </head>
 <body>
+    <?php include 'Includes/header.php'; ?>
+    <main>
+        <h1>Planten Archief</h1>
+        <div class="zoek-balk">
+            <input type="text" id="zoekInvoer" placeholder="Zoek planten...">
+        </div>
+        <div id="plantenLijst" class="planten-lijst>">
+            <!-- Planten worden hier dynamisch geladen -->
+        </div>
+    </main>
 </body>
 </html>
