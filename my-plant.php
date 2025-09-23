024a2fcc246fadf80ef38a88d9d5d34b8fbe2daa
Star <101126792+Starx707@users.noreply.github.com>
2025-09-23 13:28:48 +0200

    Some basic HTML as well as a timer in JS

---
+++ my-plant.php
@@ -0,0 +1,36 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Includes/CSS/style.css">
+    <script src="Includes/JS/my-plant.js" defer></script>
+    <title>My plant</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <p id="demo"></p>
+        <div id="infoAlarm"></div>
+        <div id="plant">
+<!--tag-->
+<!--plant-->
+<!--chair-->
+        </div>
+        <div id="interaction">
+            <h3>Joyce</h3>
+            <button>edit name</button>
+            <div>
+                <button>Water</button>
+                <button>Verander pot</button>
+                <button>Bemest</button>
+                <button>Bij knippen</button>
+                <button>Zon licht</button>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
