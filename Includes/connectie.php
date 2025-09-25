281e3862f679bcaecbd2e9d59a9a42c7b28f43d5
TrevinoRizkySunarja <trevprive123@gmail.com>
2025-09-25 13:32:14 +0200

    Correcte connectie

--- Includes/connectie.php
+++ Includes/connectie.php
@@ -2,7 +2,7 @@
 $host = "127.0.0.1";
 $user = "root";
 $password = "";
-$database = "leaflinkk";
+$database = "leaflink";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());;
