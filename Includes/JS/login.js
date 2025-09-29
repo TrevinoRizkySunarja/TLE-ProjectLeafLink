cb6283849b2abbcd8aedbb0071282f264a901258
Nydiliria <Nydiliriayt@gmail.com>
2025-09-29 11:28:37 +0200

    images and fixed registration

---
+++ Includes/JS/login.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", init)
+
+function init() { // change init when code changes
+    let h1 = document.getElementById("headerH1");
+    h1.innerText = "Login";
+}
--- login.php
+++ login.php
@@ -65,11 +65,14 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Includes/CSS/login.css">
+    <link rel="stylesheet" href="Includes/CSS/style.css">
+    <link rel="stylesheet" href="Includes/CSS/login.css">
     <title>Login</title>
+    <script src="Includes/JS/login.js"></script> 
 </head>
 <body>
-    <h1>Login</h1>
+    <?php include 'Includes/nav.php'; ?>
+    <?php include 'Includes/hoofd.php'; ?>
     <form action="" method="post">
         <div>
             <label for="username">gebruikersnaam</label>
