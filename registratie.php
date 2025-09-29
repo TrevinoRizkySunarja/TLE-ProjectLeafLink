a575976cf70a20f9d5e5879500e477ff9f023e55
Nydiliria <Nydiliriayt@gmail.com>
2025-09-29 14:33:17 +0200

    started working on styling of login

--- registratie.php
+++ registratie.php
@@ -58,7 +58,6 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./Includes/CSS/login.css">
     <title>Registratie</title>
 </head>
 <body>
