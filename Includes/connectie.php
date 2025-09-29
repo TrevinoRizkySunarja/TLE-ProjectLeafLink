a8c26728784c0621757a657f767f4eed740ec40a
chrisieee <christa_pol1@hotmail.com>
2025-09-29 10:31:29 +0200

    Nav gefixt (als het goed is)

--- Includes/connectie.php
+++ Includes/connectie.php
@@ -4,5 +4,10 @@ $user = "root";
 $password = "";
 $database = "leaflink";
 
+//$host = "stud.hosted.hr.nl";
+//$user = "0998374";
+//$password = "gaeleeph";
+//$database = "prj_2025_2026_tle1_t6";
+
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());;
