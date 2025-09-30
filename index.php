65c95d4dedccb82616e68403d1c7ca7950784930
chrisieee <christa_pol1@hotmail.com>
2025-09-30 11:51:21 +0200

    Home popup werkt en errors gefixt voor de home

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $id = $_SESSION['user']['owner_id'];
 $queryBadges = "SELECT * FROM badges WHERE owner_id = $id ORDER BY date_earned DESC LIMIT 3";
 $resultBadges = mysqli_query($db, $queryBadges)
 or die('Error ' . mysqli_error($db) . ' with query ' . $queryBadges);
-//$badges = [];
 
 if (mysqli_num_rows($resultBadges) !== 0) {
     while ($row = mysqli_fetch_assoc($resultBadges)) {
@@ -23,12 +22,21 @@ if (mysqli_num_rows($resultBadges) !== 0) {
 $queryPlants = "SELECT * FROM plants WHERE owner_id = $id LIMIT 6";
 $resultPlants = mysqli_query($db, $queryPlants)
 or die('Error ' . mysqli_error($db) . ' with query ' . $queryPlants);
-$plants = [];
 
 if (mysqli_num_rows($resultPlants) !== 0) {
     while ($row = mysqli_fetch_assoc($resultPlants)) {
         $plants[] = $row;
     }
+} else {
+    $plants = "Voeg planten toe om ze hier te zien!";
+}
+
+if (isset($_GET['id'])) {
+    $plantId = $_GET['id'];
+    $queryInfo = "SELECT * FROM plants WHERE plant_id = $plantId";
+    $resultInfo = mysqli_query($db, $queryInfo)
+    or die('Error ' . mysqli_error($db) . ' with query ' . $queryInfo);
+    $info = mysqli_fetch_assoc($resultInfo);
 }
 
 ?>
@@ -41,7 +49,6 @@ if (mysqli_num_rows($resultPlants) !== 0) {
     <link rel="stylesheet" href="Includes/CSS/style.css">
     <link rel="stylesheet" href="Includes/CSS/home.css">
     <script src="Includes/JS/home.js" defer></script>
-    <script src="Includes/JS/menu.js" defer></script>
     <title>Hoofdpagina</title>
 </head>
 <body>
@@ -57,7 +64,6 @@ if (mysqli_num_rows($resultPlants) !== 0) {
                 <p id="noBadge"><?= $badges ?></p>
             <?php } else { ?>
                 <?php foreach ($badges as $index => $badge) { ?>
-                    <!--linken naar de badgepage en de goeie badge laten zien-->
                     <a href="badges.php?badge=<?= $badge['badge_id'] ?>"><img
                                 src="Includes/images/<?= $badge['image'] ?>.png" alt=""></a>
                 <?php } ?>
@@ -66,33 +72,49 @@ if (mysqli_num_rows($resultPlants) !== 0) {
     </section>
     <section id="FavoritePlantsSec">
         <div class="shelf">
-            <?php foreach ($plants as $index => $plant) { ?>
-                <?php if ($index <= 2) { ?>
-                    <img id="<?= $plant['plant_id'] ?>" class="favoritePlant"
-                         src="Includes/images/<?= $plant['species'] ?>.png" alt="">
+            <?php if ($plants == "Voeg planten toe om ze hier te zien!") { ?>
+                <p class="noPlants"><?= $plants ?></p>
+            <?php } else { ?>
+                <?php foreach ($plants as $index => $plant) { ?>
+                    <?php if ($index <= 2) { ?>
+                        <a href="index.php?id=<?= $plant['plant_id'] ?>">
+                            <img class="favoritePlant"
+                                 src="Includes/images/<?= $plant['species'] ?>.png"
+                                 alt=""></a>
+                    <?php } ?>
                 <?php } ?>
             <?php } ?>
         </div>
         <div class="shelf">
-            <?php foreach ($plants as $index => $plant) { ?>
-                <?php if ($index > 2) { ?>
-                    <img id="<?= $plant['plant_id'] ?>" class="favoritePlant"
-                         src="Includes/images/<?= $plant['species'] ?>.png"
-                         alt="">
+            <?php if ($plants == "Voeg planten toe om ze hier te zien!") { ?>
+                <p class="noPlants"><?= $plants ?></p>
+            <?php } else { ?>
+                <?php foreach ($plants as $index => $plant) { ?>
+                    <?php if ($index > 2) { ?>
+                        <a href="index.php?id=<?= $plant['plant_id'] ?>">
+                            <img class="favoritePlant"
+                                 src="Includes/images/<?= $plant['species'] ?>.png"
+                                 alt=""></a>
+                    <?php } ?>
                 <?php } ?>
             <?php } ?>
         </div>
     </section>
     <!--Popup voor info over de plant:-->
-    <section id="PlantStatusSec">
-        <div class="border">
-            <h2>Favoriete planten</h2>
-        </div>
-        <div id="plantInfo">
-            <p>Zieke info over de plant epic!</p>
-        </div>
-        <img class="closeButton" src="Includes/images/close.png" alt="">
-    </section>
+    <?php if (isset($_GET['id'])) { ?>
+        <section id="PlantStatusSec">
+            <div class="border">
+                <h2>Favoriete planten</h2>
+            </div>
+            <div id="plantInfo">
+                <h3><?= $info['name'] ?></h3>
+                <p>Zonlicht: <?= $info['sunlight'] ?></p>
+                <p>Water behoefte: <?= $info['water_frequency'] ?></p>
+                <p>Notities: <?= $info['info'] ?></p>
+            </div>
+            <img class="closeButton" src="Includes/images/close.png" alt="">
+        </section>
+    <?php } ?>
     <section id="plantFacts"></section>
 </main>
 </body>
